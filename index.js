c46ea49c01b9737f5cc23729a14f1f8757815cfc
Danielborbafs <dev.danielborba@gmail.com>
2023-04-09 10:59:09 -0300

    middleare#1

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const app = express()
 
+app.use('/home', (req, res, next) => {
+    console.log('antes..')
+    next()
+
+})
+
+app.get('/home',(req, res,next) => {
+    console.log('durante..')
+    res.json([
+        {id: 7, name: 'Ana', position: 1 },
+        {id: 9, name: 'Dan', position: 3 }
+    ])
+next()
+})
+
+
+app.use('/home', (req, res, next) => {
+    console.log('depois..')
+    next()
+
+})
+
+
 app.listen(3000, ()  => {
     console.log('Backend executando....')
 })
